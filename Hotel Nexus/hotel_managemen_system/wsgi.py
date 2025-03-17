99122e8ccca71140e6d40bdd7e7f3cbe82555949
Golam Mahmud <52827850+Golam-Mahmud@users.noreply.github.com>
2025-03-17 16:03:20 +0600

    Update wsgi.py

--- Hotel Nexus/hotel_managemen_system/wsgi.py
+++ Hotel Nexus/hotel_managemen_system/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for hotel_managemen_system project.
+WSGI config for hotel_managemen_system project.     
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 

fd5189aeafcc065c59c18434dd2ac1d376b1a105
Golam Mahmud <52827850+Golam-Mahmud@users.noreply.github.com>
2025-03-17 17:18:07 +0600

    Update forms.py

--- Hotel Nexus/user_profile/forms.py
+++ Hotel Nexus/user_profile/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm    
 from django import forms
 
 from user_profile.models import Account, Person, Address

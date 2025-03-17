4a1c0cb30f818927d3822911dba16f1415ad6716
Golam Mahmud <52827850+Golam-Mahmud@users.noreply.github.com>
2025-03-17 16:03:49 +0600

    Update urls.py

--- Hotel Nexus/hotel_managemen_system/urls.py
+++ Hotel Nexus/hotel_managemen_system/urls.py
@@ -1,4 +1,4 @@
-"""hotel_managemen_system URL Configuration
+"""hotel_managemen_system URL Configuration    
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/

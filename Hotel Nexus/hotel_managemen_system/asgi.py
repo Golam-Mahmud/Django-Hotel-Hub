ddcf5b04259b6d771769a20928df100866b22ee1
Golam Mahmud <52827850+Golam-Mahmud@users.noreply.github.com>
2025-03-17 16:04:12 +0600

    Update asgi.py

--- Hotel Nexus/hotel_managemen_system/asgi.py
+++ Hotel Nexus/hotel_managemen_system/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for hotel_managemen_system project.
+ASGI config for hotel_managemen_system project.    
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
